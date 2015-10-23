36dc41cf1b2da7bdc540f897490a0064b334f604
sakurafan <liaodanping@gmail.com>
2015-10-23 15:55:54 -0700

    load spatial quality instead of calculating it every time

--- MATLAB/TestLive.m
+++ MATLAB/TestLive.m
@@ -10,14 +10,14 @@ if nargin < 2
     scale = 64;
 end
 if nargin < 1
-    dist_type = -2;
+    dist_type = 0;
 end
 
 %% Prepare Data
 if isunix
-    data_path = '/cs/vml2/pengp/LiveVQA';
+    data_path = '/cs/vml2/pengp/LiveVQA_1';
 else
-    data_path = '\\bluebell\vml2\pengp\LiveVQA';
+    data_path = '\\bluebell\vml2\pengp\LiveVQA_1';
 end
     
 yuv_path = fullfile(data_path, 'YUV');
@@ -30,6 +30,9 @@ frame_rates = [25 25 25 25 25 25 25 50 50 50];
 % Load the filenames of the distorted videos
 dist_names_all = importdata(fullfile(data_path, 'live_video_quality_seqs.txt'));
 total_file_num = length(dist_names_all);
+for i=1:total_file_num
+dist_names_all{i}=dist_names_all{i}(1:end-4);
+end
 subjective_data = importdata(fullfile(data_path, 'live_video_quality_data.txt'));
 dmos_all = subjective_data(:, 1);
 
@@ -40,25 +43,21 @@ if dist_type == 0 % all distortion types
     selected_videos = true(total_file_num,1);
 elseif dist_type > 0 % select a single distortion
     selected_videos = (dist_types == dist_type);
-else % exclude the distortion type 'abs(dist_type)'
-    selected_videos = (dist_types ~= abs(dist_type));
 end
 
 dmos = dmos_all(selected_videos);
-dist_filenames = dist_names(selected_videos);
+dist_filenames = dist_names_all(selected_videos);
 nfile = length(dist_filenames);
-fprintf('Subjective-evaluation device type: %s\n', mode);
+%fprintf('Subjective-evaluation device type: %s\n', mode);
 fprintf('Number of selected videos: %d\n', nfile);
 
 %% Load spatial quality (Multi-scale SSIM)
 % TODO: To be calculated
-%load(fullfile(score_path, 'live_MSSIM.mat')); 
-%spatial_scorePF_all = scoresPerFrameAll(selected_videos);
-%spatial_mScore_all = 1 - mScoreAll(selected_videos);
+load(fullfile(data_path,'spatial_mScore_all.mat'));
+spatial_mScore_all = spatial_mScore_all(selected_videos);
 
 %% Compute Overall Quality
 motion_mScore_all = zeros(nfile, 1);
-
 motion_score_folder = fullfile(score_path, strcat('motion_', int2str(scale)));
 spatial_score_folder = fullfile(score_path, 'spatial');
 if(~isdir(motion_score_folder))
@@ -84,8 +83,8 @@ for i = 1:nfile
 
     % Get per-frame spatial-quality scores ('spatial_scorePF') and the overall spatial quality score ('spatial_mScore')
     % 'spatial_mScore' is the mean value of 'spatial_scorePF' over each column
-    [spatial_mScore, spatial_scorePF] = GetQualityScores(@ComputeSpatialQuality, spatial_score_folder, yuv_path, mat_path, frame_size, ref_filename, dist_filename);
-    spatial_mScore_all(i) = 1 - spatial_mScore;
+    %[spatial_mScore, spatial_scorePF] = GetQualityScores(@ComputeSpatialQuality, spatial_score_folder, yuv_path, mat_path, frame_size, ref_filename, dist_filename);
+    %spatial_mScore_all(i) = 1 - spatial_mScore;
 end
 
 % Combine the spatial quality score and motion quality score
