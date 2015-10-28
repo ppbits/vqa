80f988b041885ca5d2d246ea5633f371ae7e04bd
ecapswen <dante.peng@gmail.com>
2015-10-27 20:12:52 -0700

    Including full scale in Live.m

--- MATLAB/Test.m
+++ MATLAB/Test.m
@@ -10,14 +10,14 @@ if strcmp(database, 'LiveMobile')
     modes = {'phone', 'tablet'};
     for m = 1:length(modes)
         WriteResultLine(modes{m}, 'a');
-        for scale = [64 128 256]
+        for scale = [64 128 256 720]
             for dist_type = [1 3 4 5 -2]
                 TestLiveMobile(modes{m}, scale, dist_type);
             end
         end
     end
 elseif strcmp(database, 'Live')
-    for scale = [64 128 256]
+    for scale = [64 128 256 432]
         for dist_type = [1 2 3 4 0]
             TestLive(scale, dist_type);
         end
